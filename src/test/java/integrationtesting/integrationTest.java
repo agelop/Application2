3c195b49e1bb995997d87093819bf027c1a74657
demo <admin@default.com>
2020-08-12 23:13:23 +0000

    Added tests

--- src/test/java/integrationtesting/integrationTest.java
+++ src/test/java/integrationtesting/integrationTest.java
@@ -1,5 +1,17 @@
 package integrationtesting;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,5 +57,29 @@ public class integrationTest {
         connection.setRequestMethod("GET");
         Assert.assertEquals(200, connection.getResponseCode());
         connection.disconnect();
+    }
+
+    @Test
+    public void integrationapitest2() throws IOException {
+        System.out.println("This is a integration api test ...");
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet("https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ");
+        HttpResponse response = httpclient.execute(httpget);
+        StatusLine status = response.getStatusLine();
+        Assert.assertEquals(HttpStatus.SC_OK, status.getStatusCode());
+        HttpEntity entity = response.getEntity();
+        String content = EntityUtils.toString(entity);
+        System.out.println(content);
+
+        JsonObject volume = new JsonParser().parse(content).getAsJsonObject();
+        Assert.assertTrue(volume.isJsonObject());
+        String expectedTitle = "Pride and Prejudice";
+        String actualTitle = volume.getAsJsonObject("volumeInfo").get("title").getAsString();
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+
+        httpget.releaseConnection();
+
+
     }
 }
